88756b9b4a9a1cdbc5bd61c513ddfb206f20d8b9
Dragon-1237 <100245626+anant-goel@users.noreply.github.com>
2022-11-13 09:25:49 +0530

    veux: Project-Elixir Bringup
    Co-Authored-By: anant-goel <100245626+anant-goel@users.noreply.github.com>

--- aosp_veux.mk
+++ aosp_veux.mk
@@ -11,18 +11,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Corvus stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_STOCK_ARCORE := false
+IS_PHONE=True
+# Gapps Flag
+USE_GAPPS := true
 
-# Evolution-X stuff.
-EVO_BUILD_TYPE := OFFICIAL
+# Official
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := Anant_Goel
 
-PRODUCT_NAME := evolution_veux
+PRODUCT_NAME := aosp_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
