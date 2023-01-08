4a7bead83383d2507bdb926a9cf5c2d3b7633890
dtyghc <100245626+anant-goel@users.noreply.github.com>
2023-01-09 03:49:03 +0530

    Veux: Bringing-UP WeedOS
    Co-Authored-By: anant-goel <100245626+anant-goel@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_veux.mk
+    $(LOCAL_DIR)/weedos_veux.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_veux-user \
-    evolution_veux-userdebug \
-    evolution_veux-eng
+    weedos_veux-user \
+    weedos_veux-userdebug \
+    weedos_veux-eng
--- weedos_veux.mk
+++ weedos_veux.mk
@@ -11,18 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common weedos stuff.
+$(call inherit-product, vendor/weedos/config/common_full_phone.mk)
 TARGET_USES_AOSP_RECOVERY := true
+WEEDOS_OFFICIAL := true
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+GAPPS_BUILD := true
 
-# Evolution-X stuff.
-EVO_BUILD_TYPE := OFFICIAL
-
-PRODUCT_NAME := evolution_veux
+PRODUCT_NAME := weedos_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
